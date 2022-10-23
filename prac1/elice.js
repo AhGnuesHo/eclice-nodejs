d583d22d8a10252968b9fce963813be8d7520f8b
seungha <frazzle124@gmail.com>
2022-10-23 14:04:50 +0900

    moduel

---
+++ prac1/elice.js
@@ -0,0 +1,13 @@
+const name = "seungha";
+const age = 25;
+const nation = "kor";
+
+//1.  모듈 내보내기
+module.exports = {
+  name1,
+  age,
+  nation,
+};
+
+//2.  키 - 값으로 모듈 사용가능
+exports.nameassss = name; //{ namessss: 'seungha' }
---
+++ prac1/require.js
@@ -0,0 +1,3 @@
+const elice = require("./elice");
+
+console.log(elice);
